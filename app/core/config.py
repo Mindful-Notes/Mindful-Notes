a76792dc627f095a4952b6afc081169ae14f7304
HHJina <jina199234@gmail.com>
2026-01-20 21:27:28 +0900

    ✨ feat: aerich init, models.py 추가, pyproject 수정
    <gitmoji> <type>: <subject>

--- app/core/config.py
+++ app/core/config.py
@@ -1,1 +1,22 @@
 # .env 관리
+# tortois
+import os
+from dotenv import load_dotenv
+
+# .env 파일의 내용을 환경 변수로 불러옵니다.
+load_dotenv()
+
+# 환경 변수에서 DATABASE_URL을 가져오고, 없으면 기본값을 사용하거나 에러를 냅니다.
+database_url = os.getenv("DATABASE_URL")
+
+TORTOISE_ORM = {
+    "connections": {
+        "default": database_url
+    },
+    "apps": {
+        "models": {
+            "models": ["app.models", "aerich.models"],
+            "default_connection": "default",
+        }
+    },
+}
