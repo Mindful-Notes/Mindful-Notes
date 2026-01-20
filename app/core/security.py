73b72cfadfba6a29cc243576fd9acb17d631bce5
RandomUsername_Kr <theoriginaldongik@gmail.com>
2026-01-20 17:46:01 +0900

    ✨ feat: 로그인 API 및 JWT 발급 구현
    <gitmoji> <type>: <subject>

--- app/core/security.py
+++ app/core/security.py
@@ -1,1 +1,35 @@
-# JWT 로직
+# fastapi
+from fastapi import FastAPI, Depends, HTTPException
+from pydantic import BaseModel
+
+# 암호화
+import jwt
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+
+
+app = FastAPI()
+
+# Password
+pwd_context = CryptContext(
+    schemes=["argon2"],
+    deprecated="auto",
+)
+
+# Hashinig
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(plain,hashed):
+    return pwd_context.verify(plain,hashed)
+
+# Access Token ( JWT 형태)
+ALGORITHM = "HS256"
+SECRET_KEY = "MysteriousSecretKey"      # 환경변수로 !!
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data:dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
